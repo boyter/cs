e52c28ac136518d8570be71c390e761679acc816
Ben Boyter <ben@boyter.org>
2019-09-04 10:07:35 +1000

    Addtional tests

--- processor/helpers_test.go
+++ processor/helpers_test.go
@@ -38,3 +38,27 @@ func TestMakeFuzzyTwo(t *testing.T) {
 		t.Error("Should get back non empty slice")
 	}
 }
+
+func TestMin(t *testing.T) {
+	got := min(0, 1)
+	if got != 0 {
+		t.Error("Expected 0 got", got)
+	}
+
+	got = min(-1, 1)
+	if got != -1 {
+		t.Error("Expected -1 got", got)
+	}
+}
+
+func TestMax(t *testing.T) {
+	got := max(0, 1)
+	if got != 1 {
+		t.Error("Expected 1 got", got)
+	}
+
+	got = max(-1, 1)
+	if got != 1 {
+		t.Error("Expected 1 got", got)
+	}
+}
