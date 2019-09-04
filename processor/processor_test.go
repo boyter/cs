ad5013f7d28226be1e73ab129929a6339dd961a4
Ben Boyter <ben@boyter.org>
2019-09-04 18:23:30 +1000

    More tests

---
+++ processor/processor_test.go
@@ -0,0 +1,26 @@
+package processor
+
+import "testing"
+
+func TestCleanSearchString(t *testing.T) {
+	SearchString = []string{
+		"AND",
+		"OR",
+		"NOT",
+		"",
+		"THE",
+	}
+	CleanSearchString()
+
+	if len(SearchString) != 4 {
+		t.Error("Expected 4")
+	}
+
+	if SearchString[3] != "the" {
+		t.Error("Expected the not THE")
+	}
+}
+
+func TestProcess(t *testing.T) {
+	Process()
+}
