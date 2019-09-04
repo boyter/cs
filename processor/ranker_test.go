f70dc06058162efeead08e5aadfbbfab0fb8a795
Ben Boyter <ben@boyter.org>
2019-09-04 10:23:21 +1000

    Add ranker test

--- processor/ranker_test.go
+++ processor/ranker_test.go
@@ -5,10 +5,30 @@ import (
 )
 
 func TestRanker(t *testing.T) {
-	results := []*FileJob{}
+
+	locations := map[string][]int{}
+	locations["test"] = []int{1,2,3}
+
+	results := []*FileJob{
+		{
+			Filename:  "test.go",
+			Extension: "go",
+			Location:  "/",
+			Content:   []byte("content"),
+			Bytes:     10,
+			Hash:      nil,
+			Binary:    false,
+			Score:     0,
+			Locations: locations,
+		},
+	}
 	ranked := RankResults(results)
 
-	if len(ranked) != 0 {
-		t.Error("Should be zero results")
+	if len(ranked) != 1 {
+		t.Error("Should be one results")
+	}
+
+	if ranked[0].Score <= 0 {
+		t.Error("Score should be greater than 0")
 	}
 }
