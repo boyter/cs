0450db3c431cc253b4677a1f6538ce69c066421f
Ben Boyter <ben@boyter.org>
2019-09-10 07:23:16 +1000

    Start to build out highlight code

--- processor/printer/standard.go
+++ processor/printer/standard.go
@@ -1,5 +1,32 @@
 package printer
 
-func WriteColored(content string, locations map[string][]int, in string, out string) {
+import (
+	"strings"
+)
 
+func WriteColored(content []byte, locations map[string][]int, in string, out string) string {
+	var str strings.Builder
+
+	end := 0
+
+	for i, x := range content {
+
+		// Check if any of the locations match
+		for key, value := range locations {
+			for _, v := range value {
+				if i == v {
+					str.WriteString(in)
+					end = v + len(key) -1
+				}
+			}
+		}
+
+		str.WriteByte(x)
+
+		if i == end {
+			str.WriteString(out)
+		}
+	}
+
+	return str.String()
 }
--- processor/printer/standard_test.go
+++ processor/printer/standard_test.go
@@ -3,5 +3,13 @@ package printer
 import "testing"
 
 func TestWriteColored(t *testing.T) {
-	WriteColored("", map[string][]int{}, "", "")
+	loc := map[string][]int{}
+	loc["this"] = []int{0}
+
+	got := WriteColored([]byte("this"), loc, "[red]", "[white]")
+
+	expected := "[red]this[white]"
+	if got != expected {
+		t.Error("Expected", expected, "got", got)
+	}
 }
