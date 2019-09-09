1baeeda295739dc3fb7649f36b784cb43679868c
Ben Boyter <ben@boyter.org>
2019-09-10 07:30:59 +1000

    Addtional test

--- processor/printer/standard_test.go
+++ processor/printer/standard_test.go
@@ -2,13 +2,25 @@ package printer
 
 import "testing"
 
-func TestWriteColored(t *testing.T) {
+func TestWriteColoredSimple(t *testing.T) {
 	loc := map[string][]int{}
 	loc["this"] = []int{0}
 
 	got := WriteColored([]byte("this"), loc, "[red]", "[white]")
 
 	expected := "[red]this[white]"
+	if got != expected {
+		t.Error("Expected", expected, "got", got)
+	}
+}
+
+func TestWriteColoredCheckInOut(t *testing.T) {
+	loc := map[string][]int{}
+	loc["this"] = []int{0}
+
+	got := WriteColored([]byte("this"), loc, "__", "__")
+
+	expected := "__this__"
 	if got != expected {
 		t.Error("Expected", expected, "got", got)
 	}
