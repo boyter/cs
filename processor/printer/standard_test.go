b89845ae7e86aad6cdcb49e0dcf0143d375480e4
Ben Boyter <ben@boyter.org>
2019-09-10 07:38:14 +1000

    Continue to test

--- processor/printer/standard_test.go
+++ processor/printer/standard_test.go
@@ -25,3 +25,27 @@ func TestWriteColoredCheckInOut(t *testing.T) {
 		t.Error("Expected", expected, "got", got)
 	}
 }
+
+func TestWriteColoredCheck2(t *testing.T) {
+	loc := map[string][]int{}
+	loc["bing"] = []int{0}
+
+	got := WriteColored([]byte("bing"), loc, "__", "__")
+
+	expected := "__bing__"
+	if got != expected {
+		t.Error("Expected", expected, "got", got)
+	}
+}
+
+func TestWriteColoredCheckTwoWords(t *testing.T) {
+	loc := map[string][]int{}
+	loc["this"] = []int{0, 5}
+
+	got := WriteColored([]byte("this this"), loc, "__", "__")
+
+	expected := "__this__ __this__"
+	if got != expected {
+		t.Error("Expected", expected, "got", got)
+	}
+}
