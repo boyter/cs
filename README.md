a8596a1d5c4d6d315465b57529fccd1ff41c2439
Ben Boyter <ben@boyter.org>
2020-02-07 08:15:06 +1100

    Im learnding

--- README.md
+++ README.md
@@ -47,3 +47,14 @@ Snippet generation
 https://stackoverflow.com/questions/2829303/given-a-document-select-a-relevant-snippet
 https://stackoverflow.com/questions/282002/c-sharp-finding-relevant-document-snippets-for-search-result-display
 
+
+https://levelup.gitconnected.com/create-your-own-expression-parser-d1f622077796
+
+
+https://blog.golang.org/normalization
+https://news.ycombinator.com/item?id=6806062
+https://groups.google.com/forum/#!topic/golang-nuts/Il2DX4xpW3w
+https://www.reddit.com/r/javascript/comments/9i455b/why_is_%C3%9Ftouppercase_equal_to_ss/
+$ rg -i --debug ß
+DEBUG|grep_regex::literal|/home/bboyter/.cargo/registry/src/github.com-1ecc6299db9ec823/grep-regex-0.1.5/src/literal.rs:59: literal prefixes detected: Literals { lits: [Complete(ß), Complete(ẞ)], limit_size: 250, limit_class: 10 }
+
--- string/common_test.go
+++ string/common_test.go
@@ -1,6 +1,10 @@
 package string
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"unicode"
+)
 
 func TestRemoveStringDuplicates(t *testing.T) {
 	r := []string{"test", "test"}
@@ -23,5 +27,64 @@ func TestPermuteCaseUnicode(t *testing.T) {
 
 	if len(permutations) != 4 {
 		t.Error("Expected 4 returns")
+	}
+}
+
+func TestPermuteCaseUnicode2(t *testing.T) {
+	permutations := PermuteCase("ſ")
+
+	if len(permutations) != 3 {
+		t.Error("Expected 3 returns")
+	}
+}
+
+func TestSimpleFoldStuff(t *testing.T) {
+	var s rune = 'ß'
+
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%#U\n", unicode.SimpleFold(s))
+		s = unicode.SimpleFold(s)
+	}
+}
+
+
+var simpleFoldTests = []string{
+	// SimpleFold(x) returns the next equivalent rune > x or wraps
+	// around to smaller values.
+
+	// Easy cases.
+	"Aa",
+	"δΔ",
+
+	// ASCII special cases.
+	"KkK",
+	"Ssſ",
+
+	// Non-ASCII special cases.
+	"ρϱΡ",
+	"ͅΙιι",
+
+	// Extra special cases: has lower/upper but no case fold.
+	"İ",
+	"ı",
+
+	// Upper comes before lower (Cherokee).
+	"\u13b0\uab80",
+}
+
+func TestSimpleFold(t *testing.T) {
+	for _, tt := range simpleFoldTests {
+		cycle := []rune(tt)
+		r := cycle[len(cycle)-1]
+		for _, out := range cycle {
+			if r := unicode.SimpleFold(r); r != out {
+				t.Errorf("SimpleFold(%#U) = %#U, want %#U", r, r, out)
+			}
+			r = out
+		}
+	}
+
+	if r := unicode.SimpleFold(-42); r != -42 {
+		t.Errorf("SimpleFold(-42) = %v, want -42", r)
 	}
 }
