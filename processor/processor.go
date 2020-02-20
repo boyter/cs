80df0d5f5e6984a3f8d68906f9bf8bb7c47491bf
Ben Boyter <ben@boyter.org>
2020-02-20 17:33:55 +1100

    Continue to improve things

--- processor/processor.go
+++ processor/processor.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-var Version = "0.0.7 alpha"
+var Version = "0.1.0 alpha"
 
 // Clean up the input to avoid searching for spaces etc...
 // Take the string cut it up, lower case everything except
@@ -61,11 +61,14 @@ func (process *Process) StartProcess() {
 	fileWalker := file.NewFileWalker(process.Directory, fileQueue)
 	fileWalker.PathExclude = PathDenylist
 	fileWalker.EnableIgnoreFile = true
+
 	fileReader := NewFileReaderWorker(fileQueue, toProcessQueue)
+
 	fileSearcher := NewSearcherWorker(toProcessQueue, summaryQueue)
 	fileSearcher.SearchString = SearchString
 	fileSearcher.MatchLimit = MatchLimit
 	fileSearcher.IncludeMinified = IncludeMinified
+
 	resultSummarizer := NewResultSummarizer(summaryQueue)
 	resultSummarizer.FileReaderWorker = &fileReader
 
--- processor/worker_summarize.go
+++ processor/worker_summarize.go
@@ -3,7 +3,7 @@ package processor
 import (
 	"fmt"
 	str "github.com/boyter/cs/string"
-	"github.com/fatih/color"
+	"strings"
 )
 
 type ResultSummarizer struct {
@@ -36,23 +36,45 @@ func (f *ResultSummarizer) Start() string {
 	// Add one here so even if we get the same value it produces some sort of result
 	rankResults2(int(f.FileReaderWorker.GetFileCount()), results)
 
-	for _, res := range results {
-		fmtBegin := "\033[1;31m"
-		fmtEnd := "\033[0m"
-		color.Magenta("%s (%.3f)", res.Location, res.Score)
+	var strb strings.Builder
+	fmtBegin := "\033[1;31m"
+	fmtEnd := "\033[0m"
 
-		// Combine all the locations such that we can highlight
-		locs := [][]int{}
+	for _, res := range results {
+		strb.WriteString(Magenta(fmt.Sprintf("%s (%.3f)\n", res.Location, res.Score)))
+
+		// Combine all the locations such that we can highlight correctly
+		l := [][]int{}
 		for _, value := range res.MatchLocations {
-			locs = append(locs, value...)
+			l = append(l, value...)
 		}
 
-		coloredContent := str.HighlightString(string(res.Content), locs, fmtBegin, fmtEnd)
-		relevant, _, _ := str.ExtractRelevant(coloredContent, locs, int(SnippetLength), str.CalculatePrevCount(int(SnippetLength), 6), "…")
+		coloredContent := str.HighlightString(string(res.Content), l, fmtBegin, fmtEnd)
+		// TODO account for cutting off the highlight portions
+		relevant, _, _ := str.ExtractRelevant(coloredContent, l, int(SnippetLength), str.CalculatePrevCount(int(SnippetLength), 6), "…")
 
-		fmt.Println(relevant)
-		fmt.Println("")
+		strb.WriteString(relevant)
+		strb.WriteString("\n\n")
 	}
 
-	return ""
+	return strb.String()
+}
+
+var (
+	Black   = Color("\033[1;30m%s\033[0m")
+	Red     = Color("\033[1;31m%s\033[0m")
+	Green   = Color("\033[1;32m%s\033[0m")
+	Yellow  = Color("\033[1;33m%s\033[0m")
+	Purple  = Color("\033[1;34m%s\033[0m")
+	Magenta = Color("\033[1;35m%s\033[0m")
+	Teal    = Color("\033[1;36m%s\033[0m")
+	White   = Color("\033[1;37m%s\033[0m")
+)
+
+func Color(colorString string) func(...interface{}) string {
+	sprint := func(args ...interface{}) string {
+		return fmt.Sprintf(colorString,
+			fmt.Sprint(args...))
+	}
+	return sprint
 }
