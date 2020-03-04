620d7d6cbde02097702cb2c608c7605a787d546f
Ben Boyter <ben@boyter.org>
2020-03-04 18:38:54 +1100

    Tweak

--- asset/memory/main.go
+++ asset/memory/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	pageSize := 1024
-	totalSize := pageSize*2_000_000
+	totalSize := pageSize*1_000_000
 	// Assume 1kb per log entry
 	// 1024 bytes to KB
 	// 1000000 means about 1GB
@@ -24,31 +24,34 @@ func main() {
 		}
 	}
 
-	start := time.Now().UnixNano() / int64(time.Millisecond)
-	for i := 0; i < len(mySlice); i += pageSize {
-		bytes.Index(mySlice[i:i+pageSize], []byte("some sort of thing"))
+	for j := 0; j < 5; j++ {
+		start := time.Now().UnixNano() / int64(time.Millisecond)
+		for i := 0; i < len(mySlice); i += pageSize {
+			bytes.Index(mySlice[i:i+pageSize], []byte("some sort of thing"))
+		}
+		fmt.Println("Single Threaded", time.Now().UnixNano()/int64(time.Millisecond)-start, "ms")
 	}
-	fmt.Println("Single Threaded", time.Now().UnixNano() / int64(time.Millisecond)-start, "ms")
 
 	// https://boyter.org/posts/my-personal-complaints-about-golang/
 	// var input = make(chan []byte, len(toProcess))
 
-	start = time.Now().UnixNano() / int64(time.Millisecond)
-	// Divide the totalsize by number of CPU
-	cpuCount := runtime.NumCPU()
-	chunk := totalSize / cpuCount
-	var wg sync.WaitGroup
-	for i := 0; i < cpuCount; i++ {
-		wg.Add(1)
+	for j := 0; j < 5; j++ {
+		start := time.Now().UnixNano() / int64(time.Millisecond)
+		cpuCount := runtime.NumCPU()
+		chunk := totalSize / cpuCount
+		var wg sync.WaitGroup
+		for i := 0; i < cpuCount; i++ {
+			wg.Add(1)
 
-		// Spawn a routine to search this chunk
-		go func(i int, j int) {
-			for k := i; k < j; k += pageSize {
-				bytes.Index(mySlice[k:k+pageSize], []byte("some sort of thing"))
-			}
-			wg.Done()
-		}(i * chunk, i * chunk + chunk)
+			// Spawn a routine to search this chunk
+			go func(i int, j int) {
+				for k := i; k < j; k += pageSize {
+					bytes.Index(mySlice[k:k+pageSize], []byte("some sort of thing"))
+				}
+				wg.Done()
+			}(i*chunk, i*chunk+chunk)
+		}
+		wg.Wait()
+		fmt.Println("Multi Threaded", time.Now().UnixNano()/int64(time.Millisecond)-start, "ms")
 	}
-	wg.Wait()
-	fmt.Println("Multi Threaded", time.Now().UnixNano() / int64(time.Millisecond)-start, "ms")
 }
