f65820da9d01562ed7689d5c864ad31b7ae374f9
Ben Boyter <ben@boyter.org>
2020-06-22 12:35:22 +1000

    woot

--- asset/tui/main.go
+++ asset/tui/main.go
@@ -15,6 +15,12 @@ type CodeResult struct {
 	BodyHeight int
 }
 
+type Result struct {
+	Title string
+	Content string
+	Score float64
+}
+
 func main() {
 	app := tview.NewApplication()
 
@@ -22,10 +28,27 @@ func main() {
 	var queryFlex *tview.Flex
 	var resultsFlex *tview.Flex
 	var overallFlex *tview.Flex
-	
-	var codeResults []CodeResult
 
-	for i:=1;i<4;i++ {
+	var codeResults []CodeResult
+	var results []Result
+
+	for i:=1;i<21;i++ {
+		results = append(results, Result{
+			Title: fmt.Sprintf(`main.go`),
+			Score: float64(i),
+			Content:  fmt.Sprintf(`func NewFlex%d() *Flex {
+	f := &Flex{
+		Box:       NewBox().SetBackgroundColor(tcell.ColorDefault),
+		direction: FlexColumn,
+	}
+	f.focus = f
+	return f
+}`, i),
+		})
+	}
+
+	// setup all of the display results
+	for i:=1;i<21;i++ {
 		var textViewTitle *tview.TextView
 		var textViewBody *tview.TextView
 
@@ -33,28 +56,16 @@ func main() {
 			SetDynamicColors(true).
 			SetRegions(true).
 			ScrollToBeginning()
-		textViewTitle.SetText(fmt.Sprintf(`[purple]main%d.go (0.0%d)[white]`, i, i))
 
 		textViewBody = tview.NewTextView().
 			SetDynamicColors(true).
 			SetRegions(true).
 			ScrollToBeginning()
 
-		textViewBody.SetText(`import (
-	"fmt"
-	"github.com/gdamore/tcell"
-	"github.com/rivo/tview"
-)
-
-type CodeResult struct {
-	Title *tview.TextView
-	Body *tview.TextView
-}`)
-
 		codeResults = append(codeResults, CodeResult{
 			Title: textViewTitle,
 			Body:  textViewBody,
-			BodyHeight: len(strings.Split(textViewBody.GetText(false), "\n")),
+			BodyHeight: 1,
 		})
 	}
 
@@ -67,6 +78,9 @@ type CodeResult struct {
 		SetFieldWidth(0).
 		SetDoneFunc(func(key tcell.Key) {
 			switch key {
+			case tcell.KeyEnter:
+				app.Stop()
+				fmt.Println(results[selected].Title)
 			case tcell.KeyTab:
 			//app.SetFocus(textView) need to change focus to the others but not the text itself
 			case tcell.KeyUp:
@@ -74,29 +88,47 @@ type CodeResult struct {
 					selected--
 				}
 
-				resultsFlex.Clear()
-				for i, t := range codeResults {
+				for _, t := range codeResults {
+					t.Title.SetText("")
+					t.Body.SetText("")
+				}
+
+				var p []Result
+				for i, t := range results {
 					if i >= selected {
-						resultsFlex.AddItem(nil, 1, 0, false)
-						resultsFlex.AddItem(t.Title, 1, 0, false)
-						resultsFlex.AddItem(nil, 1, 0, false)
-						resultsFlex.AddItem(t.Body, t.BodyHeight, 1, false)
+						p = append(p, t)
 					}
 				}
+
+				for i, t := range p {
+					codeResults[i].Title.SetText(fmt.Sprintf("[purple]%s (%f)[white]", t.Title, t.Score))
+					codeResults[i].Body.SetText(t.Content)
+					resultsFlex.ResizeItem(codeResults[i].Body, len(strings.Split(t.Content, "\n")), 0)
+				}
+
 			case tcell.KeyDown:
 				if selected != len(codeResults) -1 {
 					selected++
 				}
 
-				resultsFlex.Clear()
-				for i, t := range codeResults {
+				for _, t := range codeResults {
+					t.Title.SetText("")
+					t.Body.SetText("")
+				}
+
+				var p []Result
+				for i, t := range results {
 					if i >= selected {
-						resultsFlex.AddItem(nil, 1, 0, false)
-						resultsFlex.AddItem(t.Title, 1, 0, false)
-						resultsFlex.AddItem(nil, 1, 0, false)
-						resultsFlex.AddItem(t.Body, t.BodyHeight, 1, false)
+						p = append(p, t)
 					}
 				}
+
+				for i, t := range p {
+					codeResults[i].Title.SetText(fmt.Sprintf("[purple]%s (%f)[white]", t.Title, t.Score))
+					codeResults[i].Body.SetText(t.Content)
+					resultsFlex.ResizeItem(codeResults[i].Body, len(strings.Split(t.Content, "\n")), 0)
+				}
+
 			}
 		})
 
@@ -109,13 +141,12 @@ type CodeResult struct {
 		AddItem(queryFlex, 1, 0, false).
 		AddItem(resultsFlex, 0, 1, false)
 
-	for i, t := range codeResults {
-		if i >= selected {
-			resultsFlex.AddItem(nil, 1, 0, false)
-			resultsFlex.AddItem(t.Title, 1, 0, false)
-			resultsFlex.AddItem(nil, 1, 0, false)
-			resultsFlex.AddItem(t.Body, t.BodyHeight, 1, false)
-		}
+	// Add all of the display results into the container ready to be populated
+	for _, t := range codeResults {
+		resultsFlex.AddItem(nil, 1, 0, false)
+		resultsFlex.AddItem(t.Title, 1, 0, false)
+		resultsFlex.AddItem(nil, 1, 0, false)
+		resultsFlex.AddItem(t.Body, t.BodyHeight, 1, false)
 	}
 
 	if err := app.SetRoot(overallFlex, true).SetFocus(inputField).Run(); err != nil {
